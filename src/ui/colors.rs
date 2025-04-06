95ffa310c8ee8caf69388b2676ebc867c82fc397
DJ <dj@trunk.tools>
2025-04-06 13:03:54 -0500

    refactor colors

--- src/ui/colors.rs
+++ src/ui/colors.rs
@@ -1,19 +1,32 @@
 use macroquad::color::Color;
 
+const RED:          Color = Color { r: 255.0 / 255.0, g:   0.0 / 255.0, b:   0.0 / 255.0, a: 1.0 };
+const ORANGE:       Color = Color { r: 255.0 / 255.0, g: 127.0 / 255.0, b:   0.0 / 255.0, a: 1.0 };
+const YELLOW:       Color = Color { r: 255.0 / 255.0, g: 255.0 / 255.0, b:   0.0 / 255.0, a: 1.0 };
+const GREEN_1:      Color = Color { r: 127.0 / 255.0, g: 255.0 / 255.0, b:   0.0 / 255.0, a: 1.0 };
+const GREEN_2:      Color = Color { r:   0.0 / 255.0, g: 255.0 / 255.0, b:   0.0 / 255.0, a: 1.0 };
+const GREEN_3:      Color = Color { r:   0.0 / 255.0, g: 255.0 / 255.0, b: 127.0 / 255.0, a: 1.0 };
+const CYAN:         Color = Color { r:   0.0 / 255.0, g: 255.0 / 255.0, b: 255.0 / 255.0, a: 1.0 };
+const LIGHT_BLUE:   Color = Color { r:   0.0 / 255.0, g: 127.0 / 255.0, b: 255.0 / 255.0, a: 1.0 };
+const BLUE:         Color = Color { r:   0.0 / 255.0, g:   0.0 / 255.0, b: 255.0 / 255.0, a: 1.0 };
+const PURPLE:       Color = Color { r: 127.0 / 255.0, g:   0.0 / 255.0, b: 255.0 / 255.0, a: 1.0 };
+const MAGENTA:      Color = Color { r: 255.0 / 255.0, g:   0.0 / 255.0, b: 255.0 / 255.0, a: 1.0 };
+const RED_PINK:     Color = Color { r: 255.0 / 255.0, g:   0.0 / 255.0, b: 127.0 / 255.0, a: 1.0 };
+
 pub fn get_rainbow_color(percentage: f32) -> Color {
     let rainbow_colors: [Color; 12] = [
-        Color { r: 255.0 / 255.0, g:   0.0 / 255.0, b:   0.0 / 255.0, a: 1.0 }, // red
-        Color { r: 255.0 / 255.0, g: 127.0 / 255.0, b:   0.0 / 255.0, a: 1.0 }, // orange
-        Color { r: 255.0 / 255.0, g: 255.0 / 255.0, b:   0.0 / 255.0, a: 1.0 }, // yellow
-        Color { r: 127.0 / 255.0, g: 255.0 / 255.0, b:   0.0 / 255.0, a: 1.0 }, // green 1
-        Color { r:   0.0 / 255.0, g: 255.0 / 255.0, b:   0.0 / 255.0, a: 1.0 }, // green 2
-        Color { r:   0.0 / 255.0, g: 255.0 / 255.0, b: 127.0 / 255.0, a: 1.0 }, // green 3
-        Color { r:   0.0 / 255.0, g: 255.0 / 255.0, b: 255.0 / 255.0, a: 1.0 }, // cyan
-        Color { r:   0.0 / 255.0, g: 127.0 / 255.0, b: 255.0 / 255.0, a: 1.0 }, // light blue
-        Color { r:   0.0 / 255.0, g:   0.0 / 255.0, b: 255.0 / 255.0, a: 1.0 }, // blue
-        Color { r: 127.0 / 255.0, g:   0.0 / 255.0, b: 255.0 / 255.0, a: 1.0 }, // purple
-        Color { r: 255.0 / 255.0, g:   0.0 / 255.0, b: 255.0 / 255.0, a: 1.0 }, // magenta
-        Color { r: 255.0 / 255.0, g:   0.0 / 255.0, b: 127.0 / 255.0, a: 1.0 }, // red-pink
+        RED,
+        ORANGE,
+        YELLOW,
+        GREEN_1,
+        GREEN_2,
+        GREEN_3,
+        CYAN,
+        LIGHT_BLUE,
+        BLUE,
+        PURPLE,
+        MAGENTA,
+        RED_PINK
     ];
 
     let idx = (percentage * 11.0) as usize;
